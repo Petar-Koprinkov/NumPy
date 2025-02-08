ced12f47c4e958de7bfafe4f13d25febcc396d73
Petar Koprinkov <petur_koprinkov@abv.bg>
2025-02-09 00:12:11 +0200

    feat: Added more exercise for dimensions

--- main.py
+++ main.py
@@ -134,8 +134,26 @@ Find 7, 8, 12 in 'my_matrix'
 """
 Find 2, 3, 11, 12, 15, 15 in 'my_matrix'
 """
-#
 # print(my_matrix[[0, 3, 4], 1:])
+
+
+a_mul = np.array([
+    [
+        [1, 2, 3, 4],
+        [1, 2, 3, 4],
+        [1, 2, 3, 4],
+    ],
+    [
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+     ],
+])
+
+print(a_mul.shape)
+print(a_mul.size)
+print(a_mul.ndim)
+
 
 
 
