4d061bb1f7c4bb4b9a1e11ef2520d656aa7f566a
Petar Koprinkov <petur_koprinkov@abv.bg>
2025-02-09 00:25:04 +0200

    feat: Added more exercise

--- main.py
+++ main.py
@@ -137,22 +137,44 @@ Find 2, 3, 11, 12, 15, 15 in 'my_matrix'
 # print(my_matrix[[0, 3, 4], 1:])
 
 
-a_mul = np.array([
-    [
-        [1, 2, 3, 4],
-        [1, 2, 3, 4],
-        [1, 2, 3, 4],
-    ],
-    [
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        [13, 14, 15, 16],
-     ],
-])
+# a_mul = np.array([
+#     [
+#         [1, 2, 3, 4],
+#         [1, 2, 3, 4],
+#         [1, 2, 3, 4],
+#     ],
+#     [
+#         [5, 6, 7, 8],
+#         [9, 10, 11, 12],
+#         [13, 14, 15, 16],
+#      ],
+# ])
+#
+# print(a_mul.shape)
+# print(a_mul.size)
+# print(a_mul.ndim)
 
-print(a_mul.shape)
-print(a_mul.size)
-print(a_mul.ndim)
+# my_dict = {
+#     'one': 1,
+#     'two': 2,
+#     'three': 3,
+# }
+#
+# arr = np.array([
+#     [
+#         [1, 2, 3, 4],
+#         [1, 2, 3, 4]
+#     ],
+#     [
+#         [5, 6, my_dict, 8],
+#         [9, 10, 11, 12],
+#     ]
+# ])
+#
+#
+# print(arr.dtype)
+# print(type(arr[0, 0, 0]))
+# print(type(arr[1, 0, 2]))
 
 
 
